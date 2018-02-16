1fb4b2765e22d32c65ad1ab01d576c6f23138a7e
psykyck <clementlu@live.com>
2018-02-16 16:19:26 -0500

    add interface for interaction

--- src/main/java/com/clementlu/ticketservice/App.java
+++ src/main/java/com/clementlu/ticketservice/App.java
@@ -1,13 +1,67 @@
-package com.clementlu.TicketService;
+package com.clementlu.ticketservice;
 
-/**
- * Hello world!
- *
- */
+import java.util.Scanner;
+
+import com.clementlu.ticketservice.Constant.SeatStatus;
+import com.clementlu.ticketservice.Exceptions.InvalidVenueException;
+import com.clementlu.ticketservice.Models.SeatHold;
+import com.clementlu.ticketservice.Models.Venue;
+
 public class App 
 {
     public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    {   
+        try {
+            Scanner in = new Scanner(System.in);
+            Venue venue = new Venue(Integer.parseInt(args[0]), Integer.parseInt(args[1]));
+            
+            System.out.println("Total Seats: " + venue.getTotalSeats());
+            System.out.println("Vacant Seats: " + venue.getNumSeats(SeatStatus.VACANT));
+            System.out.println("Hold Seats: " + venue.getNumSeats(SeatStatus.HOLD));
+            System.out.println("Reserved Seats: " + venue.getNumSeats(SeatStatus.RESERVED));
+            
+            TicketService service = new PerformanceVenueTicketService(venue);
+
+            try {
+                while(true) {
+                    System.out.println("*********Start Request*********");
+                    System.out.print("Enter seats to hold: ");
+                    String numSeats = in.nextLine();
+
+                    System.out.print("Enter e-mail address: ");
+                    String email = in.nextLine();
+
+                    SeatHold seatHold = service.findAndHoldSeats(Integer.parseInt(numSeats), email);
+
+                    System.out.println("Seats Available: " + service.numSeatsAvailable());
+                    System.out.println("Vacant Seats: " + venue.getNumSeats(SeatStatus.VACANT));
+                    System.out.println("Hold Seats: " + venue.getNumSeats(SeatStatus.HOLD));
+                    System.out.println("Reserved Seats: " + venue.getNumSeats(SeatStatus.RESERVED));
+
+                    System.out.print("\nYou have 5 seconds to confirm your reservation, press ENTER to confirm...\n");
+                    in.nextLine();
+
+                    String confId = service.reserveSeats(seatHold.getId(), email);
+
+                    if (confId != null) {
+                        System.out.println("Reservation successful!");
+                        System.out.println("Confirmation Code: " + confId);
+                    } else {
+                        System.out.println("Hold expired, please try again.");
+                    }
+
+                    System.out.println("Seats Available: " + service.numSeatsAvailable());
+                    System.out.println("Vacant Seats: " + venue.getNumSeats(SeatStatus.VACANT));
+                    System.out.println("Hold Seats: " + venue.getNumSeats(SeatStatus.HOLD));
+                    System.out.println("Reserved Seats: " + venue.getNumSeats(SeatStatus.RESERVED));
+                    System.out.println("*********End Request*********");
+                }
+            } catch (Exception e) {
+                System.out.println("Restarting...");
+            }
+            
+        } catch (InvalidVenueException ex) {
+            System.out.println("Try again with valid venue dimensions: " + ex);
+        }  
     }
 }
