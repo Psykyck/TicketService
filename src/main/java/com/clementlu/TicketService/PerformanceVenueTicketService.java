fefd06ed83b950ed1267937815830c68286791ac
psykyck <clementlu@live.com>
2018-02-16 15:35:14 -0500

    add hold expiry mechanism

--- src/main/java/com/clementlu/TicketService/PerformanceVenueTicketService.java
+++ src/main/java/com/clementlu/TicketService/PerformanceVenueTicketService.java
@@ -5,13 +5,14 @@ import com.clementlu.ticketservice.Models.Seat;
 import com.clementlu.ticketservice.Models.SeatHold;
 import com.clementlu.ticketservice.Models.Venue;
 import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.UUID;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author      Clement Lu <clementlu@live.com>
@@ -23,9 +24,12 @@ public class PerformanceVenueTicketService implements TicketService {
     private Venue venue;
     private Map<Integer, SeatHold> seatHolds;
 
+    final CountDownLatch done = new CountDownLatch(1);
+    final int holdExpiry = 5;
+
     public PerformanceVenueTicketService(Venue venue) {
-        this.venue = venue;
-        seatHolds = new HashMap<Integer, SeatHold>();
+        this.venue = venue; 
+        seatHolds = new ConcurrentHashMap<Integer, SeatHold>();
     }
     
     /**
@@ -46,11 +50,17 @@ public class PerformanceVenueTicketService implements TicketService {
     * Finds best available seats in the venue and holds them.
     * <p>
     * Given {@code numSeats}, this method will attempt to hold 
-    * contiguous {@code numSeats} seats in the venue.
+    * contiguous {@code numSeats} seats in the venue. The venue 
+    * is filled with a "first fit" algorithm, starting in at (0, 0) 
+    * and moving left to right and then starting over in the first 
+    * element of the next row if there is any overflow. For example, 
+    * in a 2x2 venue, a request for a 3 seat hold would return 
+    * the following seats: [(0,0), (1,0), (0,1)].
     * <p>
-    * A successful hold will return a {@code SeatHold} with a 
-    * list of the held seats. If hold was unsuccessful, a null
-    * value will be returned.
+    * A successful hold will add the {@code SeatHold} with a 
+    * list of the held seats to {@code seatHolds}. The timer for
+    * hold expiry is also started at this time on a seperate thread. 
+    * If hold was unsuccessful, a null value will be returned.
     *
     * @param numSeats Number of seats to hold.
     * @param customerEmail Customer's email address.
@@ -79,6 +89,21 @@ public class PerformanceVenueTicketService implements TicketService {
                     venue.changeSeatStatus(seatList, SeatStatus.HOLD);
                     seatHold = new SeatHold(seatList, customerEmail);
                     seatHolds.put(seatHold.getId(), seatHold);
+                    final int seatHoldId = seatHold.getId();
+                    
+                    new Thread(new Runnable() {
+                        @Override
+                        public void run() {
+                            try {
+                                if(!done.await(holdExpiry, TimeUnit.SECONDS)) {
+                                    System.out.println("Seat hold expired.");
+                                    expiryHandler(seatHoldId);
+                                }
+                            } catch (InterruptedException iE) {
+                                System.out.println("Hold expiry thread interrupted!");
+                            }
+                        }
+                    }).start();
                 }
             }        
         }
@@ -107,11 +132,34 @@ public class PerformanceVenueTicketService implements TicketService {
             confirmationId = UUID.randomUUID().toString();
             venue.changeSeatStatus(seatHold.getSeats(), SeatStatus.RESERVED);
             seatHold.setConfirmationId(confirmationId);
+            seatHolds.remove(seatHoldId);
+            done.countDown();
         }
         return confirmationId;
     }
 
-    private void expiryHandler(SeatHold seatHold, int numSeconds) {
-
+    /**
+    * Reserves a list of seats.
+    * <p>
+    * Given {@code seatHoldId}, this method will attempt to find a
+    * {@code SeatHold} with given id and change the status of the
+    * list of seats from HOLD to RESERVED.
+    * <p>
+    * A successful reservation will return {@code confirmationId},
+    * whereas if the {@code seatHoldId} does not exist, then a null
+    * value will be returned.
+    *
+    * @param seatHoldId Id of the SeatHold containing the list of seats.
+    * @param customerEmail Customer's email address.
+    * @return Confirmation id.
+    */
+    public void expiryHandler(int seatHoldId) {
+        if (seatHolds.containsKey(seatHoldId)) {
+            Arrays.asList(seatHolds).toString();
+            List<Seat> seats = seatHolds.get(seatHoldId).getSeats();
+            seatHolds.remove(seatHoldId);
+            venue.changeSeatStatus(seats, SeatStatus.VACANT);
+            System.out.println("Seat hold " + seatHoldId + " was removed.");
+        }
     }
 }
